d03dd01a91792c37aab0e857771bfb9831eb4282
pc <a77278@alunos.uminho.pt>
2019-03-23 14:15:08 +0000

    add estruturas e metodos jogadores

--- contracts/Transferencias.sol
+++ contracts/Transferencias.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.5.0;
 contract Transferencias {
 
     address[] public participantes;
-    string  owner;
+    string public owner;
+    bytes32 public owner2;
 
     struct Jogador{
         uint id;
@@ -27,21 +28,28 @@ contract Transferencias {
         uint estadoVenda;
     }
 
-    
+    uint[] KeysJogadores;
     mapping (address => Fatura) transacoes;
     mapping (uint => Jogador) jogadores;
+    
+    //eventos teste 
+    event Exemplo(
+       string name,
+       uint age
+    );
 
 
     
     /// The smart contract's constructor
     constructor() public {
         owner = "primeiro";
+        owner2= calculateHash("primeiro");
+
+        //adicionar um jogador
+        adicionaJogador(1,20,"Carlos Gonçalves","02/01/1997");
+        adicionaJogador(2,20,"Ricardo Peixoto","01/11/1977");
     
     }
-    event Exemplo(
-       string name,
-       uint age
-    );
 
     function adicionarParticipante() public{
         participantes.push(msg.sender);
@@ -49,11 +57,29 @@ contract Transferencias {
 
     }
 
+    function adicionaJogador(uint id, uint idade, string memory nome, string  memory nascimento) private{
+            //fazer isto porque nao da para ter o tamanho dos maps aqui
+            KeysJogadores.push(id);
+            jogadores[id] = Jogador({id:id,nome:nome,idade:idade,nascimento:nascimento});
+   
+    }
+
     function getLength() public view returns (uint count) {
         return participantes.length;
     }
-    
 
+    function getJogador(uint id) public view returns (string memory count) {
+        return jogadores[id].nome;
+    }
+    
+    function getLengthJogadores() public view returns (uint num){
+        return KeysJogadores.length;
+    }
+
+    function calculateHash(string memory s )  private  returns (bytes32 res) 
+    {
+        return keccak256(abi.encodePacked(s));
+    }
 
 }
 
