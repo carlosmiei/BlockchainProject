b4cb26534428fda4ef213605750d75fc302cf052
pc <a77278@alunos.uminho.pt>
2019-05-09 08:20:47 +0100

    Tabela de infos

--- vueProject/vuebox/src/components/ConsultTrans.vue
+++ vueProject/vuebox/src/components/ConsultTrans.vue
@@ -1,5 +1,56 @@
 <template>
   <div>
+
+    <v-card>
+    <v-card-title>
+      Consultar Transações
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="transacoes"
+      :search="search"
+      :loading="false"
+    >
+      <template v-slot:items="props">
+        <td>
+          <v-layout justify-center>
+              {{ props.item.name }}
+          </v-layout>
+        </td>
+        <td class="text-xs-right">
+          <v-layout justify-center>
+          
+          {{ props.item.number }}
+         </v-layout>
+        </td>
+        <td class="text-xs-right">
+           <v-layout justify-center>
+              {{ props.item.gas }}
+          </v-layout>
+
+        </td>
+        <td class="text-xs-right"> 
+          
+          <v-layout justify-center>
+              {{ props.item.estado }}
+           </v-layout>
+        </td>
+      </template>
+      <template v-slot:no-results>
+        <v-alert :value="true" color="error" icon="warning">
+          Your search for "{{ search }}" found no results.
+        </v-alert>
+      </template>
+    </v-data-table>
+  </v-card>
        
  </div>
 </template>
@@ -10,7 +61,14 @@ export default {
     name: 'ConsultTrans',
       data () {
             return {
-              bloco:''
+              bloco:'',
+              search:'',
+               headers: [
+              {text: 'Hash da Transação',value: 'hash', align: 'center',},
+              {text: 'Bloco', value: 'bloco' },
+              {text: 'Gas', value: 'gas' },
+              {text: 'Estado', value: 'estado' }
+              ], transacoes: [ ]
             
     }
   },async created(){
@@ -20,16 +78,14 @@ export default {
       console.dir(bloco)
 
       // Ir buscar um bloco 
-      
-      
-
       for(var i=0;i<10;i++){
         var b = await this.getBlock(300-i)
         lista.push(b)
       }
       console.dir(lista)
+      lista.forEach(this.fillTable);
 
-
+      
 },methods:{
 
     getBlockNumber(){
@@ -53,9 +109,16 @@ export default {
              }
               })
           })
-    }
+    }, fillTable(elem){
+        var obj = {}
+        obj['name'] = elem.hash
+        obj['number'] = elem.number
+        obj['gas'] = elem.gasUsed
+        obj['estado'] = 'Completa'
+        this.transacoes.push(obj)
 
   }
+}
 }
 </script>
 
