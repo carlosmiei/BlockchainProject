d6fabad53b5a03f74909fdc8989d647ec9884534
pc <a77278@alunos.uminho.pt>
2019-06-13 22:56:49 +0100

    povoamento correto

--- Povoamento/povoar.js
+++ Povoamento/povoar.js
@@ -5,18 +5,19 @@ var obj;
 var cont  = fs.readFileSync('braga.json')
 
 obj = JSON.parse(cont);
-
+var i =0
 obj.forEach(function(element) {
-
-    element['_id'] = '0x685D6296B4Fd9D0F00d8CC2F634a2160B2a183A8' //ID EQUIPA
-    console.dir(element)
-    axios.post('http://localhost:4000/users/addJogador/',element)
+    var enviar ={}
+    enviar['jogador'] = element
+    enviar['_id'] = '0x685d6296b4fd9d0f00d8cc2f634a2160b2a183a8' //ID EQUIPA
+    axios.post('http://localhost:4000/users/addJogador/',enviar)
+    
     .then(response => {
         console.log('Correu tudo bem' + response)    
     
     }).catch(e => {
         console.log('ERRO: ' + e)
     })
-
+//
 
   });
