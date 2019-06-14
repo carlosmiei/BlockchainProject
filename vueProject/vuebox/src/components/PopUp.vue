17cc35b2b8e57504fbb80c8d4990a340c27812ec
ricardo <a78587@alunos.uminho.pt>
2019-06-15 00:43:44 +0100

    add Frontend pesquisa equipas/jogadores

--- vueProject/vuebox/src/components/PopUp.vue
+++ vueProject/vuebox/src/components/PopUp.vue
@@ -2,26 +2,60 @@
     <div>
 
       <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Detalhes Transaçao!   &nbsp;  <v-icon color="green" x-large >check</v-icon>
+        <div v-if="transacao">
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            Detalhes Transaçao!   &nbsp;  <v-icon color="green" x-large >check</v-icon>
 
-        </v-card-title>
+          </v-card-title>
 
-        <v-card-text>
-        <b>Id de Venda: </b> {{this.transacao._id}} <br>
-        <b>Vendedor: </b> {{this.transacao.from}} <br>
-        <b>Comprador: </b> {{this.transacao.to}} <br>
-        <b>Nome do Jogador: </b> {{this.transacao.nomeJogador}} <br>
-        <b>Valor Total / Valor Iva: </b> {{this.transacao.valorT}} &euro; /  {{this.transacao.valorI}} &euro; <br>
-        <b>Hash Transação: </b> {{this.transacao.hashTrans}} <br>
-        <b>Gas utilizado: </b> {{this.transacao.gas}} <br>
-        <b>Número da fatura: </b> {{this.transacao.numFatura}} <br>
-        <b>Número do bloco: </b> {{this.transacao.numBloco}} <br>
-        <b>Estado: </b> {{this.transacao.estado}}
-        </v-card-text>
+          <v-card-text>
+            <b>Id de Venda: </b> {{this.transacao._id}} <br>
+            <b>Vendedor: </b> {{this.transacao.from}} <br>
+            <b>Comprador: </b> {{this.transacao.to}} <br>
+            <b>Nome do Jogador: </b> {{this.transacao.nomeJogador}} <br>
+            <b>Valor Total / Valor Iva: </b> {{this.transacao.valorT}} &euro; /  {{this.transacao.valorI}} &euro; <br>
+            <b>Hash Transação: </b> {{this.transacao.hashTrans}} <br>
+            <b>Gas utilizado: </b> {{this.transacao.gas}} <br>
+            <b>Número da fatura: </b> {{this.transacao.numFatura}} <br>
+            <b>Número do bloco: </b> {{this.transacao.numBloco}} <br>
+            <b>Estado: </b> {{this.transacao.estado}}
+          </v-card-text>
+        </div>
+
+        <div v-if="jogador">
+          <v-card-title 
+            class="headline grey lighten-2"
+            primary-title
+          >
+            Detalhes do Jogador   &nbsp;  <v-icon color="green" x-large >check</v-icon>
+
+          </v-card-title>
+
+          <v-card-text>
+            <v-layout>
+              <v-flex row wrap xs12 sm7 md8 lg9>
+                <b>Nome: </b> {{ this.jogador.nome }} <br>
+                <b>Idade: </b> {{ this.idade }} anos <br>
+                <b>Nacionalidade: </b> {{ this.jogador.pais }} <br>
+                <b>Posições: </b>
+                <div v-for="pos in jogador.posicoes" :key="pos"> &nbsp;&nbsp; {{ pos }}</div> 
+              </v-flex>
+              <v-flex xs12 sm5 md4 lg43>
+                <v-img
+                    class="img"
+                    :src="jogador.foto"
+                    height="100px"
+                    width="100px"
+                    contain
+                >
+                </v-img>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </div>
 
         <v-divider></v-divider>
 
@@ -43,10 +77,14 @@ export default {
     name: 'PopUp',
       props: {
         transacao: {
-            type: Object,
-            required: true
-            // default: 'Lucas'
-   }
+          type: Object,
+          required: false
+          // default: 'Lucas'
+        },
+        jogador: {
+          type: Object,
+          required: false
+        }
     },
     components: {
 
@@ -65,9 +103,25 @@ export default {
           this.$emit('childToParent', false)
         }
     },
-    
+    computed: {
+      idade(){
+        var today = new Date();
+        var birthDate = new Date(this.jogador.nascimento);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+      }
+    }
 }
 </script>
 <style>
-    
+.img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 </style>
