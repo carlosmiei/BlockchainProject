e2c7be5e01e29ef58f86d5885a20032aedf0588b
ricardo <a78587@alunos.uminho.pt>
2019-06-21 19:41:32 +0100

    script povoação blockchain feito

--- vueProject/vuebox/populate.js
+++ vueProject/vuebox/populate.js
@@ -1,14 +1,16 @@
 var Transferencias = artifacts.require("./contracts/Transferencias.sol");
 module.exports = function (callback) {
   var transf;
-  Transferencias.deployed().then(function(deployed){
-    transf = deployed;
-    transf.adicionarEquipa("0x42C5c23F5068bf3d635857EeEBf2b4105ce35112");
-    transf.adicionarEquipa("0xdbBbfC83B5Dd0e505a6dc12D7c5F993e6c23FD46");
-    transf.adicionarEquipa("0xE0b303c8158BA5b545CbCE21Aa11FC2B62bF73E3");
-    transf.adicionarEquipa("0x335a9E8ad25Fb0d576fF5F1405CE5775370a0b4F");
-    transf.adicionarBanco("0x50C944f45603314164662310295993902121fBee");
-    transf.adicionarBanco("0xC2eF946de5Fa4CF029f0Cd6437048cdbaed8869C");
-    //transf.adicionarFederacao("0xC2eF946de5Fa4CF029f0Cd6437048cdbaed8869C");
+  Transferencias.deployed().then(function(instance){
+    transf = instance;
+    web3.eth.getAccounts().then(accounts => {
+	    transf.adicionarFederacao(accounts[1]);
+	    transf.adicionarBanco(accounts[2]);
+	    transf.adicionarBanco(accounts[3]);
+	    transf.adicionarEquipa(accounts[4]);
+	    transf.adicionarEquipa(accounts[5]);
+	    transf.adicionarEquipa(accounts[6]);
+	    transf.adicionarEquipa(accounts[7]);
+    });
   });
 }
