74fb45dfc758e2e4db860250895b2c838be72023
pc <a77278@alunos.uminho.pt>
2019-06-21 16:01:35 +0100

    contrato Limpo V2

--- vueProject/vuebox/contracts/Transferencias.sol
+++ vueProject/vuebox/contracts/Transferencias.sol
@@ -5,10 +5,8 @@ contract Transferencias {
     address[] public equipas;
     address[] public bancos;
     address[] public federacao;
-    address public tester;
+    address public admin;
     address public ownerAcc;
-    string public owner;
-    bytes32 public owner2;
 
     struct Fatura{
         uint valorTotal;
@@ -36,40 +34,6 @@ contract Transferencias {
     
     /// The smart contract's constructor
     constructor() public {
-        owner = "primeiro";
-        owner2 = calculateHash("primeiro");
-        ownerAcc = 0x24BF0ffC4daf52B0df27640882E202b0AAd98B11;
-
-        //address do endereço que corre os testes
-        tester= 0xe82f3f7C5dc295aa96e9c3b96e53998F240D426F;
-
-        
-        //adicionar os participantes
-
-        //Porto Peixoto
-        equipas.push(0x42C5c23F5068bf3d635857EeEBf2b4105ce35112);
-        //Vitória Peixoto
-        equipas.push(0xdbBbfC83B5Dd0e505a6dc12D7c5F993e6c23FD46);
-        //Nacional Peixoto
-        equipas.push(0xE0b303c8158BA5b545CbCE21Aa11FC2B62bF73E3);
-
-        //Braga Carlos
-        equipas.push(0x685D6296B4Fd9D0F00d8CC2F634a2160B2a183A8);
-
-        //Porto Carlos
-        equipas.push(0xD7E6fAB8AcFe5aa3692fDE1846b8e07E5A071eF8);
-        
-        //Bancos Peixoto
-        bancos.push(0x79e96Ee1465e7fa18A60d4B60F06226b4Ef6Ba5F);
-        bancos.push(0xF486B1d966a7ADEbA1C2a293BC2B0B384E7834d9);
-        //Bancos Carlos
-        bancos.push(0x973A5B3360bc1d5AfAFA88585eFb55F8db972Be2);
-        bancos.push(0xE2381C29Ca01ef3179fAd7d55A5C9e4fD9325461);
-
-        //Federação carlos
-        federacao.push(0x38e99B1Aa82E2310065A49fC805CC4E071CA3a79);
-        //Federação Peixoto
-        federacao.push(0xaD066d98aAe87d8d70699e3B8167fCbded72CB6F);
 
     }
 
@@ -104,8 +68,8 @@ contract Transferencias {
     }
 
     function pagaFatura(address hash) public{
-        ////require(isMember(msg.sender) == true,"Sender not authorized.");
-        ////require(transacoes[hash].estadoVenda == 3, "Operation not available");
+        require(isMember(msg.sender) == true,"Sender not authorized.");
+        require(transacoes[hash].estadoVenda == 3, "Operation not available");
         
         transacoes[hash].estadoVenda = 4;  
         emit nextStage(hash,4);
@@ -203,7 +167,7 @@ contract Transferencias {
     }
 
     function isMember(address add) public view returns (bool res) {
-        return (isTeam(add) || isBank(add) || isFederation(add) || add == ownerAcc || add == tester) ;
+        return (isTeam(add) || isBank(add) || isFederation(add) || add == ownerAcc || add == admin) ;
     }
         
     function typeA(address add) public view returns (uint res) {
