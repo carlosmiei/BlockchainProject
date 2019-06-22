45f6129d8357447002a714f61742efa59e2d254f
pc <a77278@alunos.uminho.pt>
2019-06-22 21:56:40 +0100

    script povoamento + inteligente

--- Povoamento/povoar.js
+++ Povoamento/povoar.js
@@ -1,23 +1,56 @@
 const axios = require('axios');
 const fs = require('fs');
 var obj;
+Web3 = require('web3')
+var web3;
 
-var cont  = fs.readFileSync('braga.json')
+var braga  = fs.readFileSync('braga.json')
+var porto  = fs.readFileSync('porto.json')
+var nacional  = fs.readFileSync('nacional.json')
+var guimaraes  = fs.readFileSync('guimaraes.json')
 
-obj = JSON.parse(cont);
-var i =0
-obj.forEach(function(element) {
-    var enviar ={}
-    enviar['jogador'] = element
-    enviar['_id'] = '0x685d6296b4fd9d0f00d8cc2f634a2160b2a183a8' //ID EQUIPA
-    axios.post('http://localhost:4000/users/addJogador/',enviar)
+var equipasObjeto = [JSON.parse(braga),JSON.parse(porto),JSON.parse(nacional),JSON.parse(guimaraes)]
+var equipasNome = ['braga','porto','nacional','guimaraes']
+var equipasRank = [4,5,6,7]
+
+
+if (typeof web3 !== 'undefined') {
+    console.log('Web3 injected browser: OK.')
+    Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
+
+    web3 = new Web3(web3.currentProvider)
+  } else {
+    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
     
-    .then(response => {
-        console.log('Correu tudo bem' + response)    
-    
-    }).catch(e => {
-        console.log('ERRO: ' + e)
-    })
-//
+    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  }
 
-  });
+web3.eth.getAccounts().then(accounts => {
+    for (var i in equipasNome) {
+        equipa = equipasObjeto[i];
+
+        equipa.forEach(function(element) {
+            var enviar ={}
+            enviar['jogador'] = element
+            enviar['_id'] = accounts[equipasRank[i]].toLowerCase() //ID EQUIPA
+            axios.post('http://localhost:4000/users/addJogador/',enviar)
+
+            .then(response => {
+                console.log('Correu tudo bem' + response)    
+      
+            })
+            .catch(e => {
+                console.log('ERRO: ' + e)
+             })
+
+        console.dir(enviar)
+        return
+
+
+        });
+        
+
+    }
+});
